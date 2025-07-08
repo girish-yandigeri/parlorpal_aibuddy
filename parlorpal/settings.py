763278e6e47f8c53dd9362853766e4e61c09e0b2
gallant-giri <girish4936@gmail.com>
2025-07-08 17:09:48 +0530

    Update settings.py for Railway deployment

--- parlorpal/settings.py
+++ parlorpal/settings.py
@@ -30,7 +30,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_b4!n=j-kc!u^poka&@nc#uvc$&_k7b(qpnt$4(eq&c26nbr9("
+# SECRET_KEY = "django-insecure-_b4!n=j-kc!u^poka&@nc#uvc$&_k7b(qpnt$4(eq&c26nbr9("
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -115,12 +119,22 @@ WSGI_APPLICATION = "parlorpal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
